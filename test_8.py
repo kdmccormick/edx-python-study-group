68992f1bd961942b64eca25d73f8fa912894f670
Kyle McCormick <kmccormick@edx.org>
2019-07-15 13:45:48 -0400

    More ddt

--- test_8.py
+++ test_8.py
@@ -15,3 +15,60 @@ class EnrollmentPatchTests(TestCase, EnrollmentTestMixin):
         data = [{"student_key": "bob", "status": "pending"}]
         response = requests.patch(self.allowed_url, headers=self.headers, json=data)
         self.assertEqual(response.status_code, 200)
+
+    def test_ok_multiple(self):
+        data = [
+            {"student_key": "bob", "status": "pending"},
+            {"student_key": "alice", "status": "enrolled"},
+        ]
+        response = requests.patch(self.allowed_url, headers=self.headers, json=data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_no_content(self):
+        data = []
+        response = requests.patch(self.allowed_url, headers=self.headers, json=data)
+        self.assertEqual(response.status_code, 204)
+
+    def test_multi_status_with_duplicate(self):
+        data = [
+            {"student_key": "bob", "status": "pending"},
+            {"student_key": "alice", "status": "enrolled"},
+            {"student_key": "bob", "status": "pending"},
+        ]
+        response = requests.patch(self.allowed_url, headers=self.headers, json=data)
+        self.assertEqual(response.status_code, 207)
+
+    def test_multi_status_with_invalid(self):
+        data = [
+            {"student_key": "bob", "status": "pending"},
+            {"student_key": "alice", "status": "enrolled"},
+            {"student_key": "xavier", "status": "chilling"},
+        ]
+        response = requests.patch(self.allowed_url, headers=self.headers, json=data)
+        self.assertEqual(response.status_code, 207)
+
+    def test_processing_failed(self):
+        data = [
+            {"student_key": "bob", "status": "pending"},
+            {"student_key": "bob", "status": "enrolled"},
+            {"student_key": "xavier", "status": "chilling"},
+        ]
+        response = requests.patch(self.allowed_url, headers=self.headers, json=data)
+        self.assertEqual(response.status_code, 422)
+
+    def test_bad_request_invalid_keys(self):
+        data = [
+            {"!!!": "###", "@@@": "&&&"}
+        ]
+        response = requests.patch(self.allowed_url, headers=self.headers, json=data)
+        self.assertEqual(response.status_code, 400)
+
+    def test_bad_request_not_a_dict(self):
+        data = [
+            "this is not a dict",
+            {"student_key": "bob", "status": "pending"},
+        ]
+        response = requests.patch(self.allowed_url, headers=self.headers, json=data)
+        self.assertEqual(response.status_code, 400)
+
+
