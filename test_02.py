28c5ee9a89387cac72cba6a3e3243284032fe1ac
Kyle McCormick <kmccormick@edx.org>
2019-07-16 13:09:26 -0400

    Most of ddt

--- test_02.py
+++ test_02.py
@@ -11,65 +11,48 @@ class EnrollmentPatchTests(EnrollmentTestMixin, TestCase):
 
     method = 'PATCH'
 
+    bob_enroll = {"student_key": "bob", "status": "pending"}
+    alice_enroll = {"student_key": "alice", "status": "enrolled"}
+    bad_status_enroll = {"student_key": "xavier", "status": "chilling"}
+    bad_keys_enroll = {"$tudenT<->KEY": "dora", "5TaTU5": "enrolled"}
+    bad_type_enroll = "student_key: ellie, status: pending"
+
     def test_ok(self):
-        data = [{"student_key": "bob", "status": "pending"}]
+        data = [self.bob_enroll]
         response = requests.patch(self.allowed_url, headers=self.headers, json=data)
         self.assertEqual(response.status_code, 200)
 
     def test_ok_multiple(self):
-        data = [
-            {"student_key": "bob", "status": "pending"},
-            {"student_key": "alice", "status": "enrolled"},
-        ]
+        data = [self.bob_enroll, self.alice_enroll]
         response = requests.patch(self.allowed_url, headers=self.headers, json=data)
         self.assertEqual(response.status_code, 200)
 
     def test_multi_status_with_duplicate(self):
-        data = [
-            {"student_key": "bob", "status": "pending"},
-            {"student_key": "alice", "status": "enrolled"},
-            {"student_key": "bob", "status": "pending"},
-        ]
+        data = [self.bob_enroll, self.alice_enroll, self.bob_enroll]
         response = requests.patch(self.allowed_url, headers=self.headers, json=data)
         self.assertEqual(response.status_code, 207)
 
     def test_multi_status_with_invalid(self):
-        data = [
-            {"student_key": "bob", "status": "pending"},
-            {"student_key": "alice", "status": "enrolled"},
-            {"student_key": "xavier", "status": "chilling"},
-        ]
+        data = [self.bob_enroll, self.alice_enroll, self.bad_status_enroll]
         response = requests.patch(self.allowed_url, headers=self.headers, json=data)
         self.assertEqual(response.status_code, 207)
 
     def test_processing_failed(self):
-        data = [
-            {"student_key": "xavier", "status": "chilling"},
-        ]
+        data = [self.bad_status_enroll]
         response = requests.patch(self.allowed_url, headers=self.headers, json=data)
         self.assertEqual(response.status_code, 422)
 
     def test_processing_failed_multiple(self):
-        data = [
-            {"student_key": "bob", "status": "pending"},
-            {"student_key": "bob", "status": "pending"},
-            {"student_key": "xavier", "status": "chilling"},
-        ]
+        data = [self.bob_enroll, self.bob_enroll, self.bad_status_enroll]
         response = requests.patch(self.allowed_url, headers=self.headers, json=data)
         self.assertEqual(response.status_code, 422)
 
     def test_bad_request_invalid_keys(self):
-        data = [
-            {"student_key": "alice", "status": "enrolled"},
-            {"$tudenT<->KEY": "dora", "5TaTU5": "enrolled"}
-        ]
+        data = [self.bob_enroll, self.bad_keys_enroll]
         response = requests.patch(self.allowed_url, headers=self.headers, json=data)
         self.assertEqual(response.status_code, 400)
 
     def test_bad_request_not_a_dict(self):
-        data = [
-            "student_key: ellie, status: pending",
-            {"student_key": "bob", "status": "pending"},
-        ]
+        data = [self.bad_type_enroll, self.bob_enroll]
         response = requests.patch(self.allowed_url, headers=self.headers, json=data)
         self.assertEqual(response.status_code, 400)
---
+++ test_03.py
@@ -0,0 +1,52 @@
+""" Tests, version 8, sans GET tests """
+
+from unittest import TestCase
+
+import ddt
+import requests
+
+from mixins import EnrollmentTestMixin
+
+
+@ddt.ddt
+class EnrollmentPatchTests(EnrollmentTestMixin, TestCase):
+
+    method = 'PATCH'
+
+    bob_enroll = {"student_key": "bob", "status": "pending"}
+    alice_enroll = {"student_key": "alice", "status": "enrolled"}
+    bad_status_enroll = {"student_key": "xavier", "status": "chilling"}
+    bad_keys_enroll = {"$tudenT<->KEY": "dora", "5TaTU5": "enrolled"}
+    bad_type_enroll = "student_key: ellie, status: pending"
+
+    @ddt.data(
+        [bob_enroll],  # One record
+        [bob_enroll, alice_enroll],  # Multiple records
+    )
+    def test_ok(self, data):
+        response = requests.patch(self.allowed_url, headers=self.headers, json=data)
+        self.assertEqual(response.status_code, 200)
+
+    @ddt.data(
+        [bob_enroll, alice_enroll, bob_enroll],  # Duplicate
+        [bob_enroll, alice_enroll, bad_status_enroll],  # Record with invalid status
+    )
+    def test_multi_status(self, data):
+        response = requests.patch(self.allowed_url, headers=self.headers, json=data)
+        self.assertEqual(response.status_code, 207)
+
+    @ddt.data(
+        [bad_status_enroll],  # One record
+        [bob_enroll, bob_enroll, bad_status_enroll],  # Multiple records
+    )
+    def test_processing_failed(self, data):
+        response = requests.patch(self.allowed_url, headers=self.headers, json=data)
+        self.assertEqual(response.status_code, 422)
+
+    @ddt.data(
+        [bob_enroll, bad_keys_enroll],  # Record with invalid keys
+        [bad_type_enroll, bob_enroll],  # Record that is just a string
+    )
+    def test_bad_request(self, data):
+        response = requests.patch(self.allowed_url, headers=self.headers, json=data)
+        self.assertEqual(response.status_code, 400)
